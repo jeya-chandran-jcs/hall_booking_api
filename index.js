b577633da26dd2cacd3ed350adfa3861bd4c90e3
jeya-chandran-jcs <jeyachandranjcs@gmail.com>
2024-01-15 00:56:29 +0530

    updated

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.use(express.json())
 
 const PORT = 5000;
 
-const Mongo_URL="mongodb://127.0.0.1:27017"
-// "mongodb://localhost:27017"
+const Mongo_URL="mongodb://localhost:27017"
+// "mongodb://127.0.0.1:27017"
 async function CreateConnection(){
   try{
     const client=new MongoClient(Mongo_URL)
